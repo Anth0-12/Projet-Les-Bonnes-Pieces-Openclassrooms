ee8595a5212b7da730ea5df0da3e8cf00c05cb24
Anth0-12 <antho62680@live.fr>
2023-12-01 21:38:51 +0100

    Ajout d'une fonction pour envoyer un commentaire sur l'API et l'afficher sur la fiche produit

--- avis.js
+++ avis.js
@@ -1,3 +1,4 @@
+//Afficher un commentaire
 export function ajoutListenersAvis() {
   const piecesElements = document.querySelectorAll(".fiches article button");
 
@@ -16,4 +17,27 @@ export function ajoutListenersAvis() {
       pieceElement.appendChild(avisElement);
     });
   }
+}
+
+//Envoyer un commentaire
+export function ajoutListenerEnvoyerAvis() {
+  const formulaireAvis = document.querySelector(".formulaire-avis");
+  formulaireAvis.addEventListener("submit", function (event) {
+    // Désactivation du comportement par défaut du navigateur
+    event.preventDefault();
+    // Création de l’objet du nouvel avis.
+    const avis = {
+      pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
+      utilisateur: event.target.querySelector("[name=utilisateur").value,
+      commentaire: event.target.querySelector("[name=commentaire]").value,
+    };
+    // Création de la charge utile au format JSON
+    const chargeUtile = JSON.stringify(avis);
+    // Appel de la fonction fetch avec toutes les informations nécessaires
+    fetch("http://localhost:8081/avis", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: chargeUtile,
+    });
+  });
 }
